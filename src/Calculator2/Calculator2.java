d4bff9ce58e6c620a3582a2b9aa999c32ac59e8c
vik554 <kumbharvikram27@gmail.com>
2023-05-09 15:14:17 +0530

    added to multiplicaton function

--- src/Calculator2/Calculator2.java
+++ src/Calculator2/Calculator2.java
@@ -25,5 +25,8 @@ public class Calculator2 {
     public static double subtract(double num1, double num2) {
         return num1 - num2;
     }
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
 
 }
