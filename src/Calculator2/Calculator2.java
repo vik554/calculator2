672465c6d3521df7dd240b2b13291852bd515390
vik554 <kumbharvikram27@gmail.com>
2023-05-09 15:10:13 +0530

    added subsatrction function

--- src/Calculator2/Calculator2.java
+++ src/Calculator2/Calculator2.java
@@ -22,4 +22,8 @@ public class Calculator2 {
     public static double add(double num1, double num2) {
         return num1 + num2;
     }
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
 }
