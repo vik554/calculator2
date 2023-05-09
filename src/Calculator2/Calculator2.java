e492f64073307461dc41e2613e5d19b93d006aa2
vik554 <kumbharvikram27@gmail.com>
2023-05-09 14:59:52 +0530

    code added to calculator project

---
+++ src/Calculator2/Calculator2.java
@@ -0,0 +1,21 @@
+package Calculator2;
+import java.util.Scanner;
+
+public class Calculator2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Choose an operation (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result = 0.0;
+
+    }
+}
